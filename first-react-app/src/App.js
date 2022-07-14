dc044e8bf7359bf01e85bf7fa3325fb87c12719d
ahsanjaveed <108805508+ahsanjaveed@users.noreply.github.com>
2022-07-14 11:16:19 -0700

    Class 7+8 - Async/Fetch Data

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -4,17 +4,25 @@ import Dinner from './Dinner';
 import ContextAPI from './ContextApi/contextApi';
 import CounterContext from './ContextApi/counterContext';
 import Parent from './Reducer/ParentComponent';
+import { Fetch } from './GetData/fetch';
 
 function App() {
   let countVal = useState(0)
   return (
     <div className="App">
-      <h1>Context API</h1>
-  <CounterContext.Provider value={countVal}>
-      <ContextAPI />
-  </CounterContext.Provider>
-  <h1>Reducer</h1>
-  <Parent />
+{/* Get Data */}
+<Fetch></Fetch>
+
+
+{/* Context + Reducer */}
+                    {/* <h1>Context API</h1>
+                <CounterContext.Provider value={countVal}>
+                    <ContextAPI />
+                </CounterContext.Provider>
+                <h1>Reducer</h1>
+                <Parent /> */}
+
+
     </div>
   );
 }
